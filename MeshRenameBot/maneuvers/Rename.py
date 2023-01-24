a0f64bd268976a9ff2e6e33a97520dc3c8e261c6
manybotts <105394368+manybotts@users.noreply.github.com>
2023-01-25 01:17:50 +0300

    Update Rename.py

--- MeshRenameBot/maneuvers/Rename.py
+++ MeshRenameBot/maneuvers/Rename.py
@@ -25,8 +25,7 @@ class RenameManeuver(DefaultManeuver):
     def __init__(self, client: Client, media_message: Message, cmd_message: Message) -> None:
         super().__init__(client, media_message, cmd_message)
         self._unique_id = int(str(cmd_message.chat.id)
-        self._fltr_obj = FilterUtils(cmd_message.from_user.id)
-
+        
     async def execute(self) -> None:
         self._execute_pending = False
 
